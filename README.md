a13baec2a16453cdac38c07f4ba96d4ca41d23c7
Kingsley Omage <kingsleyomage@gmail.com>
2021-09-14 16:56:18 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# phonebook-api
+  A fullstack phonebook app with a backend written in Node.js and a frontend in React.js.
---
+++ mongo.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose')
+
+if ( process.argv.length<3 ) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+
+
+const url =
+  `mongodb+srv://kingsley01:manomage@cluster0.rkk0q.mongodb.net/person-app?retryWrites=true&w=majority`
+
+mongoose.connect(url, { useNewUrlParser: true })
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String,
+
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if(process.argv[3]){
+  const name = process.argv[3]
+  const number = process.argv[4]
+  const person = new Person({
+    name: name,
+    number: number
+  })
+
+  person.save().then(response => {
+    mongoose.connection.close()
+    console.log(`added ${name} ${number} to phonebook`)
+  })
+}
+
+else{
+  
+  Person.find({}).then(result => {
+    console.log('phonebook')
+    result.forEach(person => {
+      var s =  `${person.name} ${person.number}`
+      console.log(s)
+    })
+    mongoose.connection.close()
+  })
+
+}
